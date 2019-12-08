7aef61afdb5b9de45610a36d651aa81de49a1727
RR <Roberto_Rubio98@outlook.com>
2019-12-08 00:52:08 -0600

    Added music service

---
+++ app/src/main/java/com/example/mediaplayer/MusicService.kt
@@ -0,0 +1,64 @@
+package com.example.mediaplayer
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+
+class MusicService : Service() {
+    private var position = INITIAL_POSITION
+    private val songs = arrayOf(R.raw.chop_suey, R.raw.cocaine, R.raw.painkiller)
+    private val myBinder = MyBinder()
+    private lateinit var mediaPlayer: MediaPlayer
+    override fun onBind(intent: Intent?): IBinder? {
+        return myBinder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        mediaPlayer = MediaPlayer.create(this, songs[position])
+        mediaPlayer.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.pause()
+    }
+
+    fun backwardFunction() {
+        position--
+        mediaPlayer.stop()
+        if (position < INITIAL_POSITION) {
+            position = songs.size - CONSTANT_RECEIVER
+            mediaPlayer = MediaPlayer.create(this, songs[position])
+        } else {
+            mediaPlayer = MediaPlayer.create(this, songs[position])
+        }
+        mediaPlayer.start()
+    }
+
+    fun forwardFunction() {
+        mediaPlayer.stop()
+        position++
+        if (position == songs.size) {
+            position = INITIAL_POSITION
+            mediaPlayer = MediaPlayer.create(this, songs[position])
+        } else {
+            mediaPlayer = MediaPlayer.create(this, songs[position])
+        }
+        mediaPlayer.start()
+    }
+
+    inner class MyBinder : Binder() {
+        fun getService(): MusicService {
+            return this@MusicService
+        }
+    }
+
+    companion object {
+        const val INITIAL_POSITION = 0
+        const val CONSTANT_RECEIVER = 1
+    }
+}

f9113204e3f258e234c2cefdf53f67bc2f053c96
RR <Roberto_Rubio98@outlook.com>
2019-12-08 14:47:58 -0600

    Added variables :sparkles:

--- app/src/main/java/com/example/mediaplayer/DetailSection.kt
+++ app/src/main/java/com/example/mediaplayer/DetailSection.kt
@@ -1,11 +1,18 @@
 package com.example.mediaplayer
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailSection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        val tools = Tools()
+        val song = findViewById<TextView>(R.id.band_name)
+        val infoSong = findViewById<TextView>(R.id.song_info)
+        val bundleReceived = intent.extras
+        val id = bundleReceived!!.getInt("songId")
+        song.text = tools.songName(resources.getResourceEntryName(id))
     }
 }
